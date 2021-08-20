27447bea96231b5ddc6df91477a7a5be697d8766
Jacy Mae Espinosa <jaymespinosa10@gmail.com>
2021-08-20 16:23:26 -0700

    Enter destination

--- main.py
+++ main.py
@@ -1,11 +1,13 @@
 from selenium import webdriver
 import time
 
-EMAIL = "ENTER EMAIL"
-PASSWORD = "ENTER PASSWORD"
+EMAIL = "EMAIL"
+PASSWORD = "PASSWORD"
+DESTINATION = 'Hyatt Regency Coconut Point Resort & Spa'
+# chrome_driver_path = "/Users/jacy.espinosa/Documents/chromedriver"
+# driver = webdriver.Chrome(executable_path=chrome_driver_path)
 
-
-chrome_driver_path = "ENTER PATH"
+chrome_driver_path = "/Users/jacy.espinosa/Documents/chromedriver"
 options = webdriver.ChromeOptions()
 options.add_argument("start-maximized")
 options.add_experimental_option("excludeSwitches", ["enable-automation"])
@@ -13,7 +15,7 @@ options.add_experimental_option('useAutomationExtension', False)
 driver = webdriver.Chrome(chrome_options=options, executable_path=chrome_driver_path)
 
 
-driver.get("https://www.hotels.com/profile/signin.html?target=H4sIAAAAAAAAAAXB0QkAIAgFwI30v3XCUtCMfOD63SlwazB3N2lCvGhm8H25zIVLADu7SBH-Afk_Wf4sAAAA")
+driver.get("https://www.hotels.com/")
 
 
 
@@ -27,3 +29,33 @@ def login():
 
     submit = driver.find_element_by_xpath('//*[@id="sign-in-button"]')
     submit.click()
+
+
+while True:
+    #ENTER DESTINATION INFORMATION
+    destination = driver.find_element_by_xpath('//*[@id="main"]/div[1]/div[2]/div/form/div[1]/div/div/div/div/input')
+    destination.send_keys(DESTINATION)
+
+    #CLOSE POPUP
+    try:
+        popup = driver.find_element_by_xpath('//*[@id="modal-panel-coupon-code-0"]/header/button')
+        popup.click()
+    except:
+        continue
+
+
+    #SELECT DATES
+    checkIn = driver.find_element_by_xpath('//*[@id="main"]/div[1]/div[2]/div/form/div[2]/div[1]/div[1]/div/div[1]/button')
+    next = driver.find_element_by_xpath('//*[@id="main"]/div[1]/div[2]/div/form/div[2]/div[1]/div[2]/div[1]/div/div/button[2]')
+    try:
+        if driver.find_element_by_xpath('//*[@id="main"]/div[1]/div[2]/div/form/div[2]/div[1]/div[2]/div[1]/div/div/ul/li[9]/div/h2[text()="April 2022"]'):
+            checkInDate = driver.find_element_by_xpath('//*[@id="main"]/div[1]/div[2]/div/form/div[2]/div[1]/div[2]/div[1]/div/div/ul/li[9]/div/table/tbody/tr[4]/td[1]/button')
+            checkInDate.click()
+    except:
+        next.click()
+
+
+
+
+
+
